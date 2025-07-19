9649ea5e3bcc32cb7c1630d10058821a270e2c48
sgajbi <sandeepgajbi@yahoo.com>
2025-07-19 11:39:38 +0800

    feat: Define TransactionType enum for explicit transaction types

---
+++ src/core/enums/transaction_type.py
@@ -0,0 +1,28 @@
+# src/core/enums/transaction_type.py
+
+from enum import Enum
+
+class TransactionType(str, Enum):
+    """
+    Defines the supported types of financial transactions.
+    Inheriting from 'str' ensures that the enum values are strings,
+    making them directly usable and comparable with string inputs.
+    """
+    BUY = "BUY"
+    SELL = "SELL"
+    INTEREST = "INTEREST"
+    DIVIDEND = "DIVIDEND"
+    DEPOSIT = "DEPOSIT"
+    WITHDRAWAL = "WITHDRAWAL"
+    FEE = "FEE"
+    Other = "OTHER" # Catch-all for any other transaction types not explicitly defined
+
+    @classmethod
+    def list(cls):
+        """Returns a list of all transaction type values."""
+        return list(map(lambda c: c.value, cls))
+
+    @classmethod
+    def is_valid(cls, transaction_type_str: str) -> bool:
+        """Checks if a given string is a valid transaction type."""
+        return transaction_type_str in cls.list()
